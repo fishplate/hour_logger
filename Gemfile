e626e2c5c9d808595d2f1774d74b5edae0557c84
otaiga <karl.turner5@gmail.com>
2013-01-19 10:57:56 +0000

    initial commit

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.11'
+gem 'mysql2'
+gem 'json'
+gem 'jquery-rails'
+# Authentication
+gem 'devise'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'therubyracer', '0.10.2'
+  gem 'uglifier', '>= 1.0.3'
+  gem 'twitter-bootstrap-rails'
+end
+
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'brakeman'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HourLogger::Application.config.secret_token = 'ca353bf7c3caee9d1a7d233c77e35b8df908b8b3f210c12efc50826111b73fe7d6a1a1cb0a3c0ef288b688507b0c8c4f0948a5447ac475ea136ba118b46e3063'
