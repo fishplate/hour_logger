b97a09b5a63c3fdac53eb8ab6a81c66fd9b53fac
otaiga <karl.turner5@gmail.com>
2013-01-19 19:36:18 +0000

    added user hours but tests are pending at the moment

--- spec/models/user_hour_spec.rb
+++ spec/models/user_hour_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe UserHour do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "with valid attributes and user" do
+
+    it "should be able to create user hours for user" do
+      pending ("user.new.user_hours.new etc")
+    end
+
+    it "should purge user hours for user if user is removed" do
+      pending ("remove user should remove hours")
+    end
+
+  end
 end
