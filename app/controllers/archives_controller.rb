d96c7065f4c75b4e35ced18254579b5d06e198d2
otaiga <karl.turner5@gmail.com>
2013-01-30 13:05:25 +0000

    added migration to include flags for user_hours, added methods in archived controller to group by year and view updated.

--- app/controllers/archives_controller.rb
+++ app/controllers/archives_controller.rb
@@ -2,7 +2,17 @@ class ArchivesController < ApplicationController
   before_filter :authenticate_user!
   
   def index
-    
+    @archived_hours = current_user.user_hours.archived
+    @dates = date_array(@archived_hours)
+  end
+
+
+private
+
+  def date_array(hours)
+    return unless hours
+    dates = hours.map {|h| h.date_occurred}
+    dates.group_by(&:year)
   end
 
 end
--- spec/models/user_hour_spec.rb
+++ spec/models/user_hour_spec.rb
@@ -11,6 +11,13 @@ describe UserHour do
       @user_hour.save.should == true
     end
 
+    it "should save a new user hour with archived and confirmed set to false" do
+      @user_hour = given_user.user_hours.new(@user_hour_params)
+      @user_hour.save
+      @user_hour.archived.should == false
+      @user_hour.confirmed.should == false
+    end
+
     it "should be able to get placement if valid" do
       @user_hour = given_user.user_hours.create!(@user_hour_params)
       @user_hour.placement.should == given_placement
@@ -32,6 +39,13 @@ describe UserHour do
       given_user.user_hours.create!(hours: 1.5, placement_id: given_placement.id, date_occurred: Date.parse("04/02/2013"))
       given_user.user_hours.get_date("01/02/2013").count.should == 1
       given_user.user_hours.get_date("01/02/2013").last.date_occurred.should == Date.parse("04/02/2013")
+    end
+
+    it "should be able to get al archived hours" do
+      given_user.user_hours.create!(@user_hour_params)
+      given_user.user_hours.create!(hours: 1.5, placement_id: given_placement.id, date_occurred: Date.parse("04/02/2013"), archived: true)
+      given_user.user_hours.archived.count.should == 1
+      given_user.user_hours.archived.first.date_occurred.should == Date.parse("04/02/2013")
     end
 
   end
