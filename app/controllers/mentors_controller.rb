36ebf7586ea2a4d07fbc513c854c840c9f668a7a
otaiga <karl.turner5@gmail.com>
2013-05-22 15:37:31 +0100

    Updated mentor controller to include a show method. Updated index to order by last name. Updated index view and show view. updated specs.

--- app/controllers/mentors_controller.rb
+++ app/controllers/mentors_controller.rb
@@ -2,6 +2,11 @@ class MentorsController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @users = current_user.review_users
+    @users = current_user.review_users.order(:last_name)
+  end
+
+  def show
+    user = params[:id]
+    @user = current_user.review_users.find(user)
   end
 end
--- spec/controllers/mentors_controller_spec.rb
+++ spec/controllers/mentors_controller_spec.rb
@@ -7,11 +7,24 @@ describe MentorsController do
       @user = given_user
       @user.make_mentor
       sign_in @user
+      @user2 = User.create!(
+        first_name: "test",
+        last_name: "user",
+        mentor_id: Mentor.first.id,
+        email: "test.user@example.com",
+        password: "password")
     end
 
-    it "should be able to view mentor page" do
+    it "should be able to view mentor page and review users" do
       get :index
       response.should be_success
+      assigns(:users).should == @user.review_users.order(:last_name)
+    end
+
+    it "should be able to check user hours" do
+      get :show, :id => @user2.id
+      response.should be_success
+      assigns(:user).should == @user2
     end
 
   end
