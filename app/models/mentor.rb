5067dd617da9f4a14ef2d84a78e0f66d9ea7cdf7
Karl Turner <karl.turner5@gmail.com>
2013-09-04 19:32:18 +0100

    remove mentor method (needs tests) and update views in active admin

--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -2,4 +2,8 @@ class Mentor < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :email
   has_many :users
   has_many :user_hours, :through => :users
+
+  def display_name
+    self.email
+  end
 end
